2ee00df0dc20620fa1ac0702c14a60a6b7046b2f
KU Lok Sun <sundaykuloksun@gmail.com>
2018-10-29 12:04:01 +0800

    Test array validation performance

--- index.js
+++ index.js
@@ -23,3 +23,26 @@ for (var i = 0; i <= 1000000; i++) {
   Joi.validate({ a: 'a string' }, joiObjectSchema);
 }
 console.timeEnd('JOI object schema');
+
+
+const rawArraySchema = [
+  Joi.string()
+];
+
+const joiArraySchema = Joi.array().items(
+  Joi.string()
+);
+
+console.time('Raw array schema');
+// Run it 1 million times
+for (var i = 0; i <= 1000000; i++) {
+  Joi.validate([ 'a string' ], rawArraySchema);
+}
+console.timeEnd('Raw array schema');
+
+console.time('JOI array schema');
+// Run it 1 million times
+for (var i = 0; i <= 1000000; i++) {
+  Joi.validate([ 'a string' ], joiArraySchema);
+}
+console.timeEnd('JOI array schema');
